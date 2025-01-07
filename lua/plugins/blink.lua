118cd456eeac93625b67bef47accb8b24113e1a1
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2025-01-06 22:46:13 -0700

    remove esc hide from blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -8,7 +8,6 @@ return {
       ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
       ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
       ["<CR>"] = { "accept", "fallback" },
-      ["<Esc>"] = { "hide", "fallback" },
       ["<PageUp>"] = { "scroll_documentation_up", "fallback" },
       ["<PageDown>"] = { "scroll_documentation_down", "fallback" },
     },
